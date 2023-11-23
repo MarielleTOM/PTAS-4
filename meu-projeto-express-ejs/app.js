60635ff33c8889feb49ecb670e31c4622544ec91
MarielleTOM <marielle.martins@estudante.ifms.edu.br>
2023-11-23 17:47:03 -0300

    23/11

--- meu-projeto-express-ejs/app.js
+++ meu-projeto-express-ejs/app.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 const produtos = [
-    {id: 1, nome: "Blush", preco:"196", descricao: "Kylie Cosmetics Blush compacto Kylie Janner Cor:Witer Pink", imagem: "https://www.sephora.com.br/dw/image/v2/BFJC_PRD/on/demandware.static/-/Sites-masterCatalog_Sephora/pt_BR/dw139a51a1/images/Color%20BR/KYLIE/blush/99350103304_KJC_BLUSH_21_Pink_Power_334_10g_0.35oz_Open_1500px.jpg?sw=556&sh=680&sm=fit"},
+    {id: 1, nome: "Blush", preco:"R$ 196,00", descricao: "Kylie Janner Cor:Witer Pink", imagem: "https://www.sephora.com.br/dw/image/v2/BFJC_PRD/on/demandware.static/-/Sites-masterCatalog_Sephora/pt_BR/dw139a51a1/images/Color%20BR/KYLIE/blush/99350103304_KJC_BLUSH_21_Pink_Power_334_10g_0.35oz_Open_1500px.jpg?sw=556&sh=680&sm=fit"},
     {id: 2, nome: "Lip Gloss", preco:"", descricao: "", imagem: "https://copiar--marielletom.repl.co/lipgloss.jpg"},
     {id: 3, nome: "Espuma de lavar o rosto", preco:"", descricao: "", imagem: "https://copiar--marielletom.repl.co/kylie1.jpg"},
     {id: 4, nome: "Creme Givanchy", preco:"", descricao: "", imagem: "https://copiar--marielletom.repl.co/givanchy.png"},
